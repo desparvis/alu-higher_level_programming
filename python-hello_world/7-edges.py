fb99d2a25fc02850d0ea406c7d381aa99dfd777d
desparvis <latifahm501@gmail.com>
2024-06-27 03:47:42 -0700

    r

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word([:3])
+word_first_3 = word([0:3])
 print(f"First 3 letters: {word_first_3}")
 world_last_2 = word([-2:])
 print(f"Last 2 letters: {word_last_2}")
