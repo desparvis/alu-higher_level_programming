739c248dfa636bcec65892524663879711f0b56b
desparvis <latifahm501@gmail.com>
2024-06-28 04:11:03 -0700

    r

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 def islower(c):
-    if len(c) != 1:
-        raise ValueError("Input must be a single character.")
-    return ord(c) >= 97 and ord(c) <= 122
+if len(c) != 1:
+raise ValueError("Input must be a single character.")
+return ord(c) >= 97 and ord(c) <= 122
+
+
 def main():
-    cases = ['a', 'H', 'A', '3', 'g']
-    for c in cases:
-        results = "lower" if islower(c) else "upper"
-        print("{} is {}".format(c, results))
+test_cases = ['a', 'H', 'A', '3', 'g']
+for c in test_cases:
+result = "lower" if islower(c) else "upper"
+print("{} is {}".format(c, result))
+
+
 if __name__ == "__main__":
-    main()
+main()
 ~            
