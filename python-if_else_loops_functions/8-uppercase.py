b0a105913c1357caf8be68cfda99a78054f2fcb3
desparvis <latifahm501@gmail.com>
2024-06-28 04:53:46 -0700

    r

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,11 @@ def uppercase(s):
     result = ""
     for char in s:
         if ord(char) >= 97 and ord(char) <= 122:
-        uppercase = chr(ord(char) - 32)
-        result += uppercase
-    else:
+        uppercaseu = chr(ord(char) - 32)
+        result += uppercaseu
+        else:
         result += char
-print("{}".format(result))
+    print("{}".format(result))
+if __name__ == "__main":
+    uppercase("best")
+    uppercase("Best School 98 Battery street")
