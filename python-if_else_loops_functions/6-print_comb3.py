4e2337cb32bbce603a5580e95cdbabe906ba0e49
desparvis <latifahm501@gmail.com>
2024-06-28 01:26:12 -0700

    r

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for l in range(10):
-    for k in range(l + 1, 10):
-        if l != k:
-            if l == 0 and k == 1:
-                print("{:02}".format(l * 10 + k), end="")
+for i in range(10):
+    for j in range(10):
+        if i < j:
+            if str(i) + str(j) == str(89):
+                print("{}".format(str(i) + str(j)))
             else:
-                print(", {:02}".format(l * 10 + k), end="")
+                print("{}".format(str(i) + str(j)), end=", ")
