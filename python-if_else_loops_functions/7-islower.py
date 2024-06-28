9e3565539692d3a9805ccdd2f02c84cc8ff9c1bd
desparvis <latifahm501@gmail.com>
2024-06-28 03:47:16 -0700

    r

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def islower(c):
     return ord('a') <= ord('c') <= ord('z')
+def main():
+    cases = ['a', 'H', 'A', '3', 'g']
+    for c in cases:
+        str = "lower" if slower(c) else "upper"
+        print("{} is {}".format(c, str))
+if __name__ == main:
+    main()
+~            
