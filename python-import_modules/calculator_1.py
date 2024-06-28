d7af43f86f86515f9d226528ddc70c4271a86149
desparvis <latifahm501@gmail.com>
2024-06-28 11:27:15 -0700

    r

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition"""
+    return a + b
+def sub(a, b):
+    """Subtraction"""
+    return a - b
+def mul(a, b)
+    return a * b
+def div(a,b)
+    return a / b
