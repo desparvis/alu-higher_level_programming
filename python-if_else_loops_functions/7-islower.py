540dba468667de97970325766b8a541f46185101
desparvis <latifahm501@gmail.com>
2024-06-28 03:54:10 -0700

    r

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
+    if len(c) != 1:
+        raise ValueError("Input must be a single character.")
     return ord('a') <= ord('c') <= ord('z')
 def main():
     cases = ['a', 'H', 'A', '3', 'g']
