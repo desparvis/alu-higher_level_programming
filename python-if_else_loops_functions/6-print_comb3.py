c2e5a75ab9e37dc96d876dee8658674f9fa3abdd
desparvis <latifahm501@gmail.com>
2024-06-28 01:05:48 -0700

    r

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for l in range(10):
+    for k in range(l + 1, 10):
+        if l != k:
+            if l == 0 and k == 1:
+                print("{:02}".format(l), end=", ")
+            else:
+                print("{:02}".format(l * 10 + k), end=", ")
+print("{:02}".format(9))
