5555bac0cb2029548c2b05a5b36fd7e6a24ba9a2
desparvis <latifahm501@gmail.com>
2024-06-28 15:01:23 -0700

    r

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    argv = sys.argv[1:]
+    total = 0
+    for arg in argv:
+        sum += int(arg)
+    print(sum)
+
+
+if __name__ == "__main__":
+main()
