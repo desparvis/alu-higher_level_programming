8d046e4da20224221dbde92648fd21549c3196c4
desparvis <latifahm501@gmail.com>
2024-06-28 15:14:52 -0700

    r

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 import sys
 
+if __name__ == "__main__":
 
-def main():
     argv = sys.argv[1:]
     result = 0
     for arg in argv:
         result += int(arg)
     print(result)
-if __name__ == "__main__":
-    main()
-
-
