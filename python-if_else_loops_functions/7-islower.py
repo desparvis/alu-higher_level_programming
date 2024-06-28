9cf073ebd24384b73042ff77337b6e4a326b187a
desparvis <latifahm501@gmail.com>
2024-06-28 03:11:49 -0700

    r

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     ascii = ord(c)
-    return ascii <= 97 >= 122
+    return ascii <= 97 <= 122
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
