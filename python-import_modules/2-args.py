e61cbdc1b1d87a56cfe49b3e6c979789fdaf0068
desparvis <latifahm501@gmail.com>
2024-06-28 14:18:39 -0700

    r

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    if argc == 0:
+        print("Number of arguments: 0.")
+    else :
+        print("Number of arguments: {} argument:".format(argc))
+        for i in range(argc):
+            print("{}: {}".format(i + 1, argvi[1]))
+
+if __name__ == "__main__":
+    main()
