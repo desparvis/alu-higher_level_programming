7bb791e9547fc8f362d2812bbfe9f16a61296c8a
desparvis <latifahm501@gmail.com>
2024-06-29 01:40:09 -0700

    r

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix[])):
+    for row in range(len(matrix)):
         for num in range(len(matrix[row])):
             if num == 0
                 print(" ", end='')
