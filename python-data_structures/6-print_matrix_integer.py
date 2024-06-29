53e1df7ecbbaddf9014f7734bbc358a564232f23
desparvis <latifahm501@gmail.com>
2024-06-29 01:41:15 -0700

    r

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for num in range(len(matrix[row])):
-            if num == 0
+            if num != 0
                 print(" ", end='')
             print("{:d}".format(matrix[row][num]), end='')
         print()
