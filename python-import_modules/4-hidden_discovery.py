186be47cd7fa71238e75f8345ed75be1ffad56f0
desparvis <latifahm501@gmail.com>
2024-06-28 22:29:38 -0700

    r

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 import dis
 
-if __name__ == "__main__":
-    with open('hidden_4.pyc', 'rb') as f:
-        code = f.read()
-    instructions = dis.get_instructions(code)
-    names = set()
-
-    for instr in instructions:
-        if instr.opname == 'LOAD_GLOBAL' or instr.opname == 'STORE_NAME':
-            name = instr.argval
-            if not name.startdswith('__'):
-                names.add(name)
-
-    for name in sorted(names):
-        print(name)
+if __name__ = "__main__":
+    import hidden_4
+    for item in dir(hidden_4):
+        if item[0:2] != "__":
+            print("{:s}".format(item))
