3361fa4fdf6fdedb424b6d6b8af6a5ba298c50d8
desparvis <latifahm501@gmail.com>
2024-06-29 00:01:24 -0700

    r

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_integer(my_list=[]):
+def print_reversed_list_integer(my_list=[]):
     reverse = my_list[::-1]
 
-    for num in reserve:
+    for num in reverse:
         print("{:d}".format(num))
