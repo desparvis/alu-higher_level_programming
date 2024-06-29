f39b3ec1439780c586f5dbcf3ef0467f1de5784d
desparvis <latifahm501@gmail.com>
2024-06-29 02:16:20 -0700

    r

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    a1, a2 = a[0], a[1]
+    b1, b2 = b[0], b[1]
+    sum = (a1 + b1, a2 + b2)
+    return sum
