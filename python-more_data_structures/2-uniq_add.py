e4576792b4f33225894f0b9982f7af63a9f1ef53
desparvis <latifahm501@gmail.com>
2024-06-29 06:41:11 -0700

    r

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    usum = 0
     uset = set()
     for num in my_list:
         uset.add(num)
-        sumu = sum(uset)
-    return sumu
+        usum = sum(uset)
+    return usum
