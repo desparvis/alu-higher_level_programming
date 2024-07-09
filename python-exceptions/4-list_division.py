1a5d32a9510010a6dc466778caa4ba8b1f5ea62d
desparvis <latifahm501@gmail.com>
2024-07-09 06:40:21 -0700

    r

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            if i >= len(my_list_1) or i>= len(my_list_2):
+            if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError("out of range")
             resu = my_list_1[i] / my_list_2[i]
         except TypeError:
