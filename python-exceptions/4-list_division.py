38dd76a2425cba3cda429bac71ac92ffc7dad5f2
desparvis <latifahm501@gmail.com>
2024-07-09 06:39:12 -0700

    r

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    for i in my range(list_length):
+    for i in range(list_length):
         try:
             if i >= len(my_list_1) or i>= len(my_list_2):
                 raise IndexError("out of range")
