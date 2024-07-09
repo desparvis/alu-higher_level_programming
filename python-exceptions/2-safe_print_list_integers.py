3b29ec64fa0474925b5783188a3c35310654583b
desparvis <latifahm501@gmail.com>
2024-07-09 02:09:23 -0700

    r

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,8 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             except(ValueError, TypeError):
                 continue
             except IndexError:
-                break
-    except IndexError:
         pass
         print()
         return count
