e110ba5120a602a9c518537195049190d3318e7d
desparvis <latifahm501@gmail.com>
2024-07-12 02:07:00 -0700

    r

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -3,6 +3,7 @@
 class here
 """
 
+
 class Square:
     """
     class docs
@@ -21,7 +22,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         return self.__size ** 2
 
