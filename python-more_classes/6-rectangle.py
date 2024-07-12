f1fbeb766ecf1ebe45621e7b43f844d793ac485c
desparvis <latifahm501@gmail.com>
2024-07-12 12:45:35 -0700

    r

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,10 +9,12 @@ class Rectangle:
     class rectangle
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+
     @property
     def width(self):
         return self.__width
