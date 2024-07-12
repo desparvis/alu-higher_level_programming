7c8c16b20d8c6c32c12b1fddeb807a2b8ef23951
desparvis <latifahm501@gmail.com>
2024-07-12 10:39:08 -0700

    r

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-module is being documented
+module first
 """
+
 
 class Rectangle:
     """
@@ -43,4 +44,4 @@ class Rectangle:
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height
+        return 2 * (self.__width + self.__height)
