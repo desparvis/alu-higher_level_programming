02067b07f2778bea972e9ecf78127146d1e708aa
desparvis <latifahm501@gmail.com>
2024-07-12 01:09:51 -0700

    r

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,12 +8,13 @@ class Square:
     """
     class stuff mens
     """
-    
     def __init__(self, size=0):
         self.size = size
     @property
+
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -22,5 +23,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
         return self.__size ** 2
