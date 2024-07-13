fa1fa5c93f104a0fdfc859afc29168bd64515ec8
desparvis <latifahm501@gmail.com>
2024-07-13 13:50:09 -0700

    r

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that inherits from Rectangle."""
+    def __init__(self, size):
+        """Instantiation with size."""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        print(f"[Square] {self.__size}/{self.__size}")
