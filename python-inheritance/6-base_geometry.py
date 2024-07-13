aa1c29af84f98bac8fff31f76efc85220eb58de1
desparvis <latifahm501@gmail.com>
2024-07-13 11:32:32 -0700

    r

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+defining class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    this is class
+    """
+    def area(self):
+        """
+        raising except and stuff
+        """
+        raise Exception("area() is not implemented")
