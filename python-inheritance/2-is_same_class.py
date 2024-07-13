7f82c9e17d153567407251ec2e709948487fb151
desparvis <latifahm501@gmail.com>
2024-07-13 10:43:10 -0700

    r

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-modules
+define is_same_class function
 """
 
 
 def def is_same_class(obj, a_class):
     """
-    function
+    check if a obj is an instance of a class
     """
     return type(obj) is a_class
