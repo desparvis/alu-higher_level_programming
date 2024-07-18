0a1c5a409b6048d5e6ce07ca1c7d14cb53dd9c14
desparvis <latifahm501@gmail.com>
2024-07-18 05:52:43 -0700

    r

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+module is well  documented
+"""
+
+
 def read_file(filename=""):
+    """
+    function is well documented
+    """
     with open(filename, 'r', encoding='utf-8'):
         result = file.read()
         print(result)
