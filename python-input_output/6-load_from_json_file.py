ef2d0b81215e063d5a4044d91dc070ee8c1394ee
desparvis <latifahm501@gmail.com>
2024-07-19 07:51:25 -0700

    r

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,4 +9,5 @@ def load_from_json_file(filename):
     """
     well defined function
     """
-    return json.load(filename)
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
