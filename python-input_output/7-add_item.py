d5bd9c3c09ece38cd5c977cc7a52fa86171cb0f1
desparvis <latifahm501@gmail.com>
2024-07-19 08:52:06 -0700

    r

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,10 +5,10 @@ this module is well documenting
 from sys import argv
 
 
-save = __import__("5-save_to_json_file").save_to_json_file
-load = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 filename = "add_item.json"
-json_list = load(filename)
+json_list = load_from_json_file(filename)
 for i in argv[1:]:
     json_list.append(i)
-save(json_list, filename)
+save_to_json_file(json_list, filename)
