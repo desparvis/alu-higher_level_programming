70e5d211ba8c2b8c2d3db585dc0f79b757edd5df
desparvis <latifahm501@gmail.com>
2024-07-19 08:56:26 -0700

    r

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """
-this module is well documenting
+This module adds command-line arguments to a JSON file.
 """
-from sys import argv
 
+from sys import argv
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
 filename = "add_item.json"
-json_list = load_from_json_file(filename)
+
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
+
 for i in argv[1:]:
     json_list.append(i)
+
 save_to_json_file(json_list, filename)
