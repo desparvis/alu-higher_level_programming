f7e229135447839ca1b080e55cae3c2c420556a3
desparvis <latifahm501@gmail.com>
2024-07-19 03:02:47 -0700

    r

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module is well documented
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    function is well documented
+    """
+    return json.loads(my_str)
