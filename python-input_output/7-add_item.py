6a718bf3342ba30954fc8770c803d66f95c23358
desparvis <latifahm501@gmail.com>
2024-07-19 08:29:54 -0700

    r

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,7 +3,6 @@
 module needs documenting
 """
 
-
 from sys import argv
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file.py").load_from_json_file
