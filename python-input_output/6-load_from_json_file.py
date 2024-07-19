d3cf52f25f6030e5913969720fb481d79e0bdbb8
desparvis <latifahm501@gmail.com>
2024-07-19 07:46:32 -0700

    r

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+well documented module
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    well defined function
+    """
+    return json.load(filename)
