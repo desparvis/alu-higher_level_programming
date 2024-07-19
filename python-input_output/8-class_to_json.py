0051ee14bdbf01ca81b5d48551160ef0b71a0d3e
desparvis <latifahm501@gmail.com>
2024-07-19 10:18:53 -0700

    r

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -8,5 +8,4 @@ def def class_to_json(obj):
     """
     function is well documented
     """
-    return {key: value for key, value in obj.__dict__.items()
-            if isinstance(value, (list, dict, str, int, bool))}
+    return obj.__dict__
