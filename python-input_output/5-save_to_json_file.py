f64e41dd9971f68ba64eba26bb9b085dcfc56375
desparvis <latifahm501@gmail.com>
2024-07-19 07:20:54 -0700

    r

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,10 +5,11 @@ document module importing json
 import json
 
 
-def save_to_json_file(my_obj, filename) as a:
+def save_to_json_file(my_obj, filename:
     """
     document function writing file using JSON
     """
-    with open(filename, 'w', encoding="utf-8"):
-        json.dump(my_obj, a)
+    with open(filename, 'w', encoding="utf-8") as a:
+        result = json.dumps(my_obj)
+        a.write(result)
         
