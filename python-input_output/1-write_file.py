07177d19b91384c9b0bf0d87cceb288c240ca8f4
desparvis <latifahm501@gmail.com>
2024-07-19 01:23:11 -0700

    r

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+module is well documented
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    function is well documented
+    """
     with open(filename, "w", encoding="utf-8") as file:
         result = file.write(text)
     return result
