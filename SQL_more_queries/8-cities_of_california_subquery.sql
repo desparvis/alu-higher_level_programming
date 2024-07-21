b1aee8691e591da5c683e528bf171d67e88c98b4
desparvis <latifahm501@gmail.com>
2024-07-21 06:51:30 -0700

    r

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- cities of california
-SELECT id, name FROM states WHERE state_id IN (SELECT id FROM states WHERE name="California") ORDER BY id ASC;
+SELECT id, name FROM states WHERE state_id IN (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
