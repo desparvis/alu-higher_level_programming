7cd54c395d504e853a5d3897450d19a50999c4f1
desparvis <latifahm501@gmail.com>
2024-07-21 12:12:50 -0700

    r

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- man life is so sad bro
-SELECT TV_SHOW_GENRES.genre AS genre, COUNT(tv_shows.id) AS number_of _shows FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id GROUP BY tv_show_genres>genre HAVING number_of_shows > 0 ORDER BY number_of_shows DESC;
+SELECT TV_SHOW_GENRES.genre AS genre, COUNT(tv_shows.id) AS number_of_shows FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id GROUP BY tv_show_genres>genre HAVING number_of_shows > 0 ORDER BY number_of_shows DESC;
