720440f541f7ac4cc69aad7bfa9eac30fb88f0c7
desparvis <latifahm501@gmail.com>
2024-07-21 05:26:38 -0700

    r

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- create db and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	name VARCHAR(256) NOT NULL));
+	name VARCHAR(256) NOT NULL );
