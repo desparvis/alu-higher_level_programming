63a9db33e9960d1d348d743280f319def7ba7ffc
desparvis <latifahm501@gmail.com>
2024-08-09 09:28:37 -0700

    r

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
-'''Using the SQL Alchemy in defining a database class'''
+"""
+This script contains the class definition of a State and an instance
+Base = declarative_base(). It connects to a MySQL server running on localhost
+at port 3306.
+"""
 
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+
 Base = declarative_base()
 
+class State(Base):
+    """
+    State class that represents the 'states' table in MySQL database.
 
-class City(Base):
-    '''I function'''
-    __tablename__ = 'cities'
+    Attributes:
+        id (int): Primary key, auto-incremented, non-nullable.
+        name (str): String of max 128 characters, non-nullable.
+    """
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+
+if __name__ == "__main__":
+    # Replace 'your_username', 'your_password', and 'your_database' with actual values
+    engine = create_engine('mysql+mysqldb://your_username:your_password@localhost/your_database',
+                           pool_pre_ping=True)
+
+    # Create all tables in the engine (essentially, create the 'states' table)
+    Base.metadata.create_all(engine)
