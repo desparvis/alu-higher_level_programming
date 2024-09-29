fad23de3ca849d2d026fef4e5e2473615a2c2532
desparvis <latifahm501@gmail.com>
2024-09-29 04:41:39 -0700

    4concat

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,6 +2,8 @@
 const args = process.argv;
 if (args[2] === undefined ||  args[3] === undefined) {
   console.log('undefined is undefined');
+} else if (args[3] === undefined) {
+  console.log(`${args[2]} is `);
 } else {
   console.log(`${args[2]} is ${args[3]}`);
 }
