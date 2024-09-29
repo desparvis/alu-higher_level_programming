eeeae2674af1c15ce09cfa2fe205853dc7b08763
desparvis <latifahm501@gmail.com>
2024-09-29 09:32:32 -0700

    r

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,9 +4,9 @@ const x = parseInt(args[0], 10);
 if (isNaN(x)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < x; i++) {
     let line = '';
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < x; j++) {
       line += 'X';
     }
     console.log(line);
