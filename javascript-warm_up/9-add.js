62e8f97c6ac1143073d83609ff673c37a6f1d80a
desparvis <latifahm501@gmail.com>
2024-09-29 10:48:48 -0700

    r

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-return a + b;
+function add (a, b) {
+  return a + b;
 }
 const args = process.argv.slice(2);
 const num1 = parseInt(args[0], 10);
@@ -8,5 +8,5 @@ const num2 = parseInt(args[1], 10);
 if (isNaN(num1) || isNaN(num2)) {
   console.log('NaN');
 } else {
-console.log(add(num1, num2));
+  console.log(add(num1, num2));
 }
