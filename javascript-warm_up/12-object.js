7ee99fe81f6c590cf969a6b4a4eb9cd34419a974
desparvis <latifahm501@gmail.com>
2024-09-29 11:20:31 -0700

    r

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// Your code here: Replacing 12 with 89
+myObject.value = 89;
+
+console.log(myObject);
