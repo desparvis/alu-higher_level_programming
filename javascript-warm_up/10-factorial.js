00f082a557770e7ea8ebfa816aeee9acbc0831de
desparvis <latifahm501@gmail.com>
2024-09-29 11:01:17 -0700

    r

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+const args = process.argv.slice(2);
+const num = parseInt(args[0], 10);
+
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
