f19aa75c04b0f355e83977793b2602ae1547a146
desparvis <latifahm501@gmail.com>
2024-09-29 05:39:40 -0700

    5 integer

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args[0] === undefined) {
+  console.log('Not a number');
+} else {
+  const num = parseInt(args[0], 10);
+  if (isNaN(num)) {
+    console.log('Not a number');
+  } else {
+    console.log(`My number : ${num}`);
+  }
+}
