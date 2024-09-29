7c4aa40a16c0a7e0847a0114ac9d8b854a195d78
desparvis <latifahm501@gmail.com>
2024-09-29 07:59:06 -0700

    r

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const x = parseInt(args[0], 10);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i<x; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
