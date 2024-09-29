52caaec401d20035f03f0da4e2e20f4b3beb4dc7
desparvis <latifahm501@gmail.com>
2024-09-29 04:33:07 -0700

    r

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args[2] === undefined ||  args[3] === undefined {
+  console.log('undefined is undefined');
+} else {
+  console.log(`${args[2]} is ${args[3]}`);
+}
