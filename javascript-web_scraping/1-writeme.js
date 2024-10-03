ba28cfb83a7112a090ddb405df377a29aa2a8109
desparvis <latifahm501@gmail.com>
2024-10-03 10:04:13 +0000

    r

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-// Check if both arguments are provided, otherwise do nothing
-if (filePath && stringToWrite) {
-  fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
-} else {
-  // Log the arguments for debugging
-  console.error('File path or string to write is missing.');
-}
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
