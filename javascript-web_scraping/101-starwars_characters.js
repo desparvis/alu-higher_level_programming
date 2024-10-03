68741d246dfbed92adf54d7622816ce99ebef28a
desparvis <latifahm501@gmail.com>
2024-10-03 11:07:14 +0000

    r

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,38 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2]; 
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Usage: node script.js <movie_id>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  if (data.title) {
+    const characters = data.characters;
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (err, response, body) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const characterData = JSON.parse(body);
+        console.log(characterData.name);
+      });
+    });
+  } else {
+    console.log('Movie not found or invalid ID.');
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
