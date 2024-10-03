ff9e3a5ebbc2a4d04e920862d67df52582a06ce8
desparvis <latifahm501@gmail.com>
2024-10-03 09:42:00 +0000

    r

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,14 +4,11 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
-if (!filePath || !stringToWrite) {
-  console.log('Usage: node script.js <file_path> <string_to_write>');
-  process.exit(1);
+// Check if both arguments are provided, otherwise do nothing
+if (filePath && stringToWrite) {
+  fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 }
-
-fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-});
